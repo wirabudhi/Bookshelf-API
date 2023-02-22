d496f025da91cf930132702dce8fc002d2a6181a
Wira Budhi <bpantaiselatan@gmail.com>
2023-02-22 12:23:31 +0800

    initial commit

---
+++ src/books.js
@@ -0,0 +1,5 @@
+// Mendefinisikan variabel untuk menampung array
+const books = [];
+
+// Melakukan exports module
+module.exports = books;
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+// Melakukan import
+const {
+    addBookHandler,
+    getAllBookHandler,
+    getBookByIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler,
+} = require('./handler');
+
+// Melakukan routing
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getBookByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: editBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBookByIdHandler,
+    },
+];
+
+// Melakukan exports module
+module.exports = routes;
